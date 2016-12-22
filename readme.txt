018b8090b0290c36da07f4120525828574e02dcb
yehaicao <yehaicao@gmail.com>
2016-12-22 10:33:08 +0800

    修正了可能无法分享到腾讯的社交媒体的问题

--- readme.txt
+++ readme.txt
@@ -1,4 +1,4 @@
-中文免费WordPressLeaf主题 1.0.0
+中文免费WordPressLeaf主题 1.1.1
 
 WordPressLeaf是一个干净、具有现代感、完全响应式设计的免费的wordpress主题。
 
--- template-parts/content-share.php
+++ template-parts/content-share.php
@@ -4,18 +4,14 @@
 			<a rel="nofollow" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&amp;url=<?php echo esc_url( get_permalink() ); ?>" class="popup">
 				<span class="rrssb-icon">
 					<i class="fa fa-twitter"></i>
-
 				</span>
 				<span class="rrssb-text">Twitter</span>
 			</a>
 		</li>
-
 		<li class="rrssb-facebook" >
 			<a rel="nofollow" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo esc_url( get_permalink() ); ?>" class="popup">
 				<span class="rrssb-icon">
-
 					<i class="fa fa-facebook-square"></i>
-
 				</span>
 				<span class="rrssb-text">Facebook</span>
 			</a>
@@ -24,43 +20,37 @@
 			<a rel="nofollow" href="https://plus.google.com/share?url=<?php echo esc_url( get_permalink() ); ?>" class="popup">
 				<span class="rrssb-icon">
 					<i class="fa fa-google-plus"></i>
-
 				</span>
 				<span class="rrssb-text">Google+</span>
 			</a></li>
 			<li class="rrssb-pinterest" >
-				<a rel="nofollow" href="http://pinterest.com/pin/create/button/?url=<?php echo esc_url( get_permalink() ); ?>&amp;description=<?php echo strip_tags(codilight_lite_excerpt(60)); ?>&amp;media=<?php if ( has_post_thumbnail( ) ) { the_post_thumbnail_url('codilight_lite_single_medium' );} ?>">
+				<a rel="nofollow" href="http://pinterest.com/pin/create/button/?url=<?php echo esc_url( get_permalink() ); ?>&amp;media=<?php if ( has_post_thumbnail( ) ) { the_post_thumbnail_url('codilight_lite_single_medium' );} ?>&amp;description=<?php echo strip_tags(codilight_lite_excerpt(60)); ?>" class="popup">
 					<span class="rrssb-icon">
 						<i class="fa fa-pinterest"></i>
-
 					</span>
 					<span class="rrssb-text">Pinterest</span>
 				</a>
 			</li>
 			<li class="rrssb-tumblr" >
-				<a rel="nofollow" href="http://tumblr.com/share/link?url=<?php echo esc_url( get_permalink() ); ?>&amp;name=<?php the_title(); ?>">
+				<a rel="nofollow" href="http://tumblr.com/share/link?url=<?php echo esc_url( get_permalink() ); ?>&amp;name=<?php the_title(); ?>" class="popup">
 					<span class="rrssb-icon">
 						<i class="fa fa-tumblr"></i>
-
 					</span>
 					<span class="rrssb-text">Tumblr</span>
 				</a>
 			</li>
 			<li class="rrssb-linkedin small" >
-				<a rel="nofollow" href="http://www.linkedin.com/shareArticle?mini=true&amp;url=<?php echo esc_url( get_permalink() ); ?>&amp;title=<?php the_title(); ?>&amp;summary=<?php echo strip_tags(codilight_lite_excerpt(60)); ?>" class="popup">
+				<a rel="nofollow" href="http://www.linkedin.com/shareArticle?url=<?php echo esc_url( get_permalink() ); ?>&amp;title=<?php the_title(); ?>&amp;summary=<?php echo strip_tags(codilight_lite_excerpt(60)); ?>&amp;mini=true" class="popup">
 					<span class="rrssb-icon">
 						<i class="fa fa-linkedin"></i>
-
 					</span>
 					<span class="rrssb-text">Linkedin</span>
 				</a>
 			</li>
 			<li class="rrssb-reddit small" >
-				<a rel="nofollow" href="http://www.reddit.com/submit?url=<?php echo esc_url( get_permalink() ); ?>&amp;title=<?php the_title(); ?>&amp;text=<?php echo strip_tags(codilight_lite_excerpt(60)); ?>">
+				<a rel="nofollow" href="http://www.reddit.com/submit?url=<?php echo esc_url( get_permalink() ); ?>&amp;title=<?php the_title(); ?>&amp;text=<?php echo strip_tags(codilight_lite_excerpt(60)); ?>" class="popup">
 					<span class="rrssb-icon">
 						<i class="fa fa-reddit"></i>
-
-
 					</span>
 					<span class="rrssb-text">Reddit</span>
 				</a>
@@ -69,7 +59,6 @@
 				<a rel="nofollow" href="mailto:?subject=<?php the_title(); ?>&body=<?php echo esc_url( get_permalink() ); ?>">
 					<span class="rrssb-icon">
 						<i class="fa fa-envelope"></i>
-
 					</span>
 					<span class="rrssb-text">Email</span>
 				</a>
